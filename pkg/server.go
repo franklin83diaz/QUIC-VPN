1dab651aefc4022ae4b6e2abd8647d6608fdc3cd
Franklin Diaz <franklin83diaz@gmail.com>
2024-05-14 23:49:54 -0400

    multiclient

--- pkg/server.go
+++ pkg/server.go
@@ -21,25 +21,27 @@ func Server() {
 		log.Fatal(err)
 	}
 	defer listener.Close()
+	for {
+		conn, err := listener.Accept(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// Accept a QUIC session
-	conn, err := listener.Accept(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
+		go func() {
+			stream, err := conn.AcceptStream(context.Background())
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	stream, err := conn.AcceptStream(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
+			defer stream.Close()
 
-	defer stream.Close()
+			data := make([]byte, 1500)
+			for n, _ := stream.Read(data); n > 0; n, _ = stream.Read(data) {
 
-	data := make([]byte, 1500)
-	for n, _ := stream.Read(data); n > 0; n, _ = stream.Read(data) {
-
-		fmt.Printf("\033[32mServer: Received '%s'\033[0m\n", data)
-		time.Sleep(1 * time.Second)
+				fmt.Printf("\033[32mServer: Received '%s'\033[0m\n", data)
+				time.Sleep(1 * time.Second)
+			}
+		}()
 	}
 
 }
